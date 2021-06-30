7635e4f8cce3f343b54f379b57eed249a1b26186
Qi Ming <phoenixqm@163.com>
2021-06-30 18:12:18 +0800

    add opencv win ex code

---
+++ opencv-win/LearningOpenCV-win/ex2-1/ex2-1.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+	cv::Mat img = cv::imread(argv[1], -1);
+	if (img.empty()) {
+		return -1;
+	}
+	cv::namedWindow("ex2-1", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Demo Image", img);
+	cv::waitKey(0);
+	cv::destroyWindow("ex2-1");
+	return 0;
+}
+
---
+++ opencv-win/LearningOpenCV-win/ex2-3/ex2-3.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char** argv)
+{
+	cv::namedWindow("ex2-3", cv::WINDOW_AUTOSIZE);
+	cv::VideoCapture cap;
+	cap.open(std::string(argv[1]));
+
+	cv::Mat frame;
+	while(true) {
+		cap >> frame;
+		if (frame.empty()) {
+			break;
+		}
+		cv::imshow("Demo Video", frame);
+		if (cv::waitKey(33) >= 0) {
+			break;
+		}
+	}
+
+	return 0;
+}
+
